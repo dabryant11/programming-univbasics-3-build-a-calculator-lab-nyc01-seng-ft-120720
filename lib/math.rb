794a70f891b8b87fb872f11ab7cc49a6a8cf82fe
darien bryant <darienbryant11@gmail.com>
2020-11-25 22:34:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,31 +1,36 @@
 def addition(num1, num2)
 result = num1 + num2
 puts result
+result
 end
 
 def subtraction(num1, num2)
   result = num1 - num2
   puts result
-
+  result
 end
 
 def division(num1, num2)
   result = num1 / num2
   puts result
+  result
 end
 
 def multiplication(num1, num2)
   result = num1 * num2
   puts result
+  result
 end
 
 def modulo(num1, num2)
   result = num1 % num2
   puts result
+  result
 end
 
 def square_root(num)
   result = num**
   puts result 
+  result
   
 end
